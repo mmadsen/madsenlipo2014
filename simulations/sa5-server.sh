a791d12b0aee98db0b851e2cc9302eed4e51ff40
Mark Madsen <mark@madsenlab.org>
2014-04-22 09:44:14 -0700

    Everything in the archive....

--- simulations/sa5-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ssh ec2-user@ec2-54-196-201-32.compute-1.amazonaws.com -i ~/Dropbox/keystore/dissertation.pem
--- simulations/sa8-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ssh ec2-user@ec2-54-225-28-155.compute-1.amazonaws.com -i ~/Dropbox/keystore/dissertation.pem
